5f8a9eb2b0ba7c52f5a5c687502e035c25462578
Jeff Lapp <lappjeff20@gmail.com>
2019-05-15 20:38:38 -0400

    POST functionality added for zoos. Adding telephones isn't working yet and I still need to convert to using postgres from h2, but I'll do that in the morning

--- src/main/java/zoos/zoos/controller/AdminController.java
+++ src/main/java/zoos/zoos/controller/AdminController.java
@@ -1,11 +1,17 @@
 package zoos.zoos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import zoos.zoos.model.Zoo;
 import zoos.zoos.service.ZooService;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping(value = "/admin")
@@ -23,6 +29,19 @@ public class AdminController
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
+	//localhost:2018/admin/zoos
+	@PostMapping(value = "/zoos", produces = {"application/json"}, consumes = {"application/json"})
+	public ResponseEntity<?> addNewZoo(@Valid @RequestBody Zoo newZoo) throws URISyntaxException
+	{
+		newZoo = zooService.save(newZoo);
+		HttpHeaders responseHeaders = new HttpHeaders();
+		URI newZooURI = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{zooname}").
+				buildAndExpand(newZoo.getZooname()).toUri();
+		responseHeaders.setLocation(newZooURI);
+
+		return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+	}
 	//localhost:2018/admin/zoos/{id}
 	@DeleteMapping(value = "/zoos/{zooid}")
 	public ResponseEntity<?> deleteZooById(@PathVariable long zooid)
